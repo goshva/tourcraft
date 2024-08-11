a24a0170b5525891daabde4a4b3ce87c03d55556
goshva <sha.egor@gmail.com>
2024-08-11 12:50:50 +0300

    fix build

---
+++ src/App.vue.js
@@ -0,0 +1,44 @@
+import TinderCards from './components/TinderCards.vue';
+const { defineProps, defineSlots, defineEmits, defineExpose, defineModel, defineOptions, withDefaults, } = await import('vue');
+const __VLS_fnComponent = (await import('vue')).defineComponent({});
+;
+let __VLS_functionalComponentProps;
+function __VLS_template() {
+    let __VLS_ctx;
+    /* Components */
+    let __VLS_otherComponents;
+    let __VLS_own;
+    let __VLS_localComponents;
+    let __VLS_components;
+    let __VLS_styleScopedClasses;
+    // CSS variable injection 
+    // CSS variable injection end 
+    let __VLS_resolvedLocalAndGlobalComponents;
+    // @ts-ignore
+    [TinderCards,];
+    // @ts-ignore
+    const __VLS_0 = __VLS_asFunctionalComponent(TinderCards, new TinderCards({ msg: ("Vite + Vue"), }));
+    const __VLS_1 = __VLS_0({ msg: ("Vite + Vue"), }, ...__VLS_functionalComponentArgsRest(__VLS_0));
+    ({}({ msg: ("Vite + Vue"), }));
+    const __VLS_4 = __VLS_nonNullable(__VLS_pickFunctionalComponentCtx(TinderCards, __VLS_1));
+    if (typeof __VLS_styleScopedClasses === 'object' && !Array.isArray(__VLS_styleScopedClasses)) {
+    }
+    var __VLS_slots;
+    return __VLS_slots;
+    const __VLS_componentsOption = {};
+    let __VLS_name;
+    let __VLS_defineComponent;
+    const __VLS_internalComponent = __VLS_defineComponent({
+        setup() {
+            return {
+                TinderCards: TinderCards,
+            };
+        },
+    });
+}
+export default (await import('vue')).defineComponent({
+    setup() {
+        return {};
+    },
+});
+;
---
+++ vite.config.js
@@ -0,0 +1,29 @@
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import { VitePWA } from 'vite-plugin-pwa';
+export default defineConfig({
+    plugins: [
+        vue(),
+        VitePWA({
+            registerType: 'autoUpdate',
+            manifest: {
+                name: 'My Vue PWA',
+                short_name: 'VuePWA',
+                description: 'My Vue.js Progressive Web App',
+                theme_color: '#ffffff',
+                icons: [
+                    {
+                        src: 'pwa-192x192.png',
+                        sizes: '192x192',
+                        type: 'image/png'
+                    },
+                    {
+                        src: 'pwa-512x512.png',
+                        sizes: '512x512',
+                        type: 'image/png'
+                    }
+                ]
+            }
+        })
+    ]
+});
